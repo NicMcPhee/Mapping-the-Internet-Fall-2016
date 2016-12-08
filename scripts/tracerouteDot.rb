9108d7713d60a92b6f8a3ece58f2e1eab026c97e
J-Ziegler <ziegl210@morris.umn.edu>
2016-12-08 11:22:37 -0600

    Fix the Ruby Script Kyle Broke

--- scripts/tracerouteDot.rb
+++ scripts/tracerouteDot.rb
@@ -65,7 +65,7 @@ def parse_entries_to_dot(output_file, entries, entry_regex)
   end
 end
 
-output_file_name = "antimicrobial_network_graph.dot"
+output_file_name = "network_graph.dot" # DON'T PUSH THIS FILE IF YOU UPDATE THIS LINE
 output_file = File.open(output_file_name, "w")
 
 print_header(output_file)
